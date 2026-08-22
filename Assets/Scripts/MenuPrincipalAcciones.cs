7d5f59ed43df69335111d158b467087a0bb24c8d
Luchy <lucianaoviedodanielawanuffelen@gmail.com>
2026-08-22 01:30:26 -0300

    PANTALLA ELEGIR JUGADOR (UNO O DOS) + BTN SALIR OK

--- Assets/Scripts/MenuPrincipalAcciones.cs
+++ Assets/Scripts/MenuPrincipalAcciones.cs
@@ -5,11 +5,22 @@ public class MenuPrincipalAcciones : MonoBehaviour
 {
     [SerializeField] private GameObject menuPrincipalPanel;
     [SerializeField] private GameObject comoJugarPanel;
+    [SerializeField] private GameObject elegirModoPanel;
 
     // Cargar la escena del juego por su nombre exacto 
     public void IniciarJuego(string nombreEscena)
     {
         SceneManager.LoadScene(nombreEscena);
+    }
+
+    // Llamar desde el OnClick del botón "INICIAR JUEGO"
+    public void MostrarModoJuego()
+    {
+        if (menuPrincipalPanel != null)
+            menuPrincipalPanel.SetActive(false);
+
+        if (elegirModoPanel != null)
+            elegirModoPanel.SetActive(true);
     }
 
     // Llamar desde el OnClick del botón "Como_jugar"
@@ -20,6 +31,9 @@ public class MenuPrincipalAcciones : MonoBehaviour
 
         if (comoJugarPanel != null)
             comoJugarPanel.SetActive(true);
+
+        if (elegirModoPanel != null)
+            elegirModoPanel.SetActive(true);
     }
 
     // Útil para el botón "Volver" en el panel ComoJugar
@@ -30,11 +44,19 @@ public class MenuPrincipalAcciones : MonoBehaviour
 
         if (comoJugarPanel != null)
             comoJugarPanel.SetActive(false);
+
+        if (elegirModoPanel != null)
+            elegirModoPanel.SetActive(false);
     }
 
     public void SalirDelJuego()
     {
         Debug.Log("Saliendo del juego...");
         Application.Quit();
+
+        // Detiene el modo Play si estás dentro del editor de Unity
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
